d50e53a19eea429e89fe8c173441fd2e36fc84ef
GeraNuno <l21212008@tectijuana.edu.mx>
2022-05-24 11:20:20 -0700

    Numero entero al cuadrado
    Programa creado el 24/05/2022, donde se implementan excepciones.

---
+++ NumeroEntero/NumeroEntero/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroEntero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            try
+            {
+                Console.Write("Escribe un número entero: ");
+                num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nEl cuadrado de " + num + " es: " + Math.Pow(num,2));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+            Console.WriteLine("\nPresione ENTER para salir del programa...");
+            Console.ReadKey();
+        }
+    }
+}
